319f52a9dbb96d75e0ed4e96eec4fac75c9b0a3a
TariqKichawele <tariqkichawele66@gmail.com>
2024-10-18 16:12:55 -0400

    Implemented generateChaptersWithAI function using openai. Implemented a parsing utility function

--- app/generate-chapters/action.ts
+++ app/generate-chapters/action.ts
@@ -3,7 +3,9 @@
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "../api/auth/[...nextauth]/route"
 import { validateYoutubeUrl } from "@/utils/validation";
-import { getVideoId } from "@/utils/youtube";
+import { getVideoDetails, getVideoId, getVideoTranscript } from "@/utils/youtube";
+import { parseXMLContent } from "@/utils/parsing";
+import { generateChaptersWithAI } from "@/utils/openai";
 
 type GenerateChaptersResponse = {
     success: boolean;
@@ -30,6 +32,38 @@ export async function generateChapters(formData: FormData): Promise<GenerateChap
     if(!videoId) {
         return { success: false, error: "Failed to get video ID" }
     }
+
+    const videoDetails = await getVideoDetails(videoId);
+    const videoTranscript = await getVideoTranscript(videoId);
+    
+    if(!videoDetails || !videoTranscript?.subtitles || videoTranscript.subtitles.length === 0) {
+        return { success: false, error: "Failed to get video details" }
+    }
+
+    const lengthSeconds = 
+        typeof videoDetails.lengthSeconds === "string" 
+        ? parseInt(videoDetails.lengthSeconds, 10) 
+        : videoDetails.lengthSeconds;
+
+    if(isNaN(lengthSeconds)) {
+        return { success: false, error: "Failed to get video length" }
+    }
+
+    if(lengthSeconds > 3600) {
+        return { success: false, error: "Video length is too long" }
+    }
+
+    const parsedTranscript = await parseXMLContent(videoTranscript.subtitles[0]);
+    if(!parsedTranscript) { 
+        return { success: false, error: "Failed to parse transcript" }
+    }
+
+    const openAIChapters = await generateChaptersWithAI(parsedTranscript, lengthSeconds);
+    if(!openAIChapters) {
+        return { success: false, error: "Failed to generate chapters" }
+    }
+
+    
 
 
 
